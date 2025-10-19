b59f4b446b58608eee438b7a88b841e16296ea4a
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2025-10-19 20:16:42 -0300

    inicializei o frontend com o vite e adicionei os endpoints para controlar os leds corretamente

---
+++ client/src/components/LedControl.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button @click="toggleLed" :disabled="loading">
+    {{ ledOn ? "Desligar LED" : "Ligar LED" }} 
+  <button>
+
+</template>
--- server/main.py
+++ server/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from consumer import start_consumer_thread
 from shared import received_messages
+import RPi.GPIO as GPIO
 
 start_consumer_thread()
 
 app = FastAPI()
-
+LED_PIN = 18
 origins = ["http://localhost:8080"]
 
 app.add_middleware(
@@ -24,3 +25,14 @@ async def get_data():
 async def post_data(data: dict):
     received_messages.append(data)
     return {"status": "received"}
+
+# ENDPOINTS PARA FAZER O CONTROLE DOS LEDS.
+@app.post("/led/on")
+def led_on():
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    return { "message": "LED LIGADO" }
+
+@app.post("/led/off")
+def led_off():
+    GPIO.output(LED_PIN, GPIO.LOW)
+    return { "message" : "LED DESLIGADO" }
